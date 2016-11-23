662d042d740c152a06b18cecbbd3d86c2f755b0f
Alex Roessner <alex.roessner@gmail.com>
2016-11-23 13:49:04 -0700

    Some cleanup

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -2,7 +2,7 @@ class Voter < ApplicationRecord
   has_many :question_choices, dependent: :destroy
 
   validates :session_id, presence: true
-  validates :zip_id, presence: true
+  validates :zip_id, length: { minimum: 5 }
 
   accepts_nested_attributes_for :question_choices, allow_destroy: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,25 @@
 Rails.application.routes.draw do
   resources :question_choices
-  resources :choices
-  resources :decisions
+  #resources :choices
+  #resources :decisions
   resources :judges
-  resources :questions
-  resources :stances
-  resources :referendas
-  resources :voters
-  resources :zips
-  resources :ballots
-  resources :divisions
-  resources :precincts
-  resources :cities
-  resources :counties
-  resources :states
-  resources :countries
-  resources :terms
-  resources :candidates
+  #resources :questions
+  #resources :stances
+  resources :referendas, except: [:edit, :destroy]
+  resources :voters, except: [:index, :show, :edit, :destroy]
+  #resources :zips
+  #resources :ballots
+  #resources :divisions
+  #resources :precincts
+  #resources :cities
+  #resources :counties
+  #resources :states
+  #resources :countries
+  #resources :terms
+  resources :candidates, except: [:edit, :new, :destroy]
   get 'match_results', to: 'home#match_results', as: 'match_results'
   get 'match_start', to: 'voters#new', as: 'match_start'
+  #match 'match_start', to: "voters#new", as: 'match_start', via: [:get, :post]
   get 'match', to: 'question_choices#new', as: 'match'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
